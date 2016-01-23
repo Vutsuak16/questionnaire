6ec60543dd19bff6ada8b17ecb6dbc162705bce3
kaustuv deolal <vutsuak96@gmail.com>
2016-01-24 02:52:20 +0530

    optimization for prime factors

--- largest_prime_factor.py
+++ largest_prime_factor.py
@@ -1,18 +1,17 @@
 __name__ = "vutsuak"
 
-n = 15
+n = 600851475143
 
 prime_factors = []
-for i in range(1, n + 1):
+for i in range(1, int((n + 1)**.5+1)):
     if n % i == 0:
         ct = 0
         if i > 1:
             sqrt_factor = i ** .5
-            print(sqrt_factor)
             for j in range(1, int(sqrt_factor)+1):
                 if i % j == 0:
                     ct += 1
             if ct == 1:
                 prime_factors.append(i)
 
-print prime_factors
+print max(prime_factors)
